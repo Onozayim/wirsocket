0f41e46e7f3b673f65e14dab677174d87416cfa3
Onozayim <camilomdo@gmail.com>
2022-04-18 13:01:54 -0500

    cors

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 
 const io = require("socket.io")(server, {
   cors: {
-    origin: "https://wirhir.netlify.app//llamadas/server",
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
